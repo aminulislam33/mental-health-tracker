838eae18a9f6bd3801a6d3482eba5e1a20577e10
Aminul Islam <aminulislam03905@gmail.com>
2025-01-11 10:15:48 +0530

    [START]: started the project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+module.exports = app;
